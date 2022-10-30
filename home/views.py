ec3354a51908c24decbb44e40d23e0e00b364821
DESKTOP-DBNQ0KF\user <hdj1196@gmail.com>
2022-10-30 19:09:22 +0900

    html로 페이지 연결을 위한 파일명 변경

--- home/views.py
+++ home/views.py
@@ -3,11 +3,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'home_index.html')
 
 
 def video(request):
-    return render(request, 'video.html')
+    return render(request, 'index.html')
 
 
 def intro(request):
